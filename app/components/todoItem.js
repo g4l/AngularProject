b2bf66aaa165c8891812417deddc0019a65c1e0f
Dmitriy Osetrov <dosetrov@yahoo.com>
2016-11-08 00:23:49 +0200

    Added methods for complete todo and editing todo

--- app/components/todoItem.js
+++ app/components/todoItem.js
@@ -4,5 +4,25 @@ angular.module('manageBoard')
         bindings: {
             todo: '<'
         },
-        templateUrl: 'app/components/templates/todoitem.html'
+        templateUrl: 'app/components/templates/todoitem.html',
+        controller: function(todosService) {
+            this.showEdit = false;
+            this.completed = false;
+            this.enableEdit = () => {
+                this.showEdit = !this.showEdit;
+            }
+
+            this.deleteTodo = () => {
+                todosService.completeTodo(this.todo._id).then(() => {
+                    this.completed = true;
+                    this.showEdit = false;
+                });
+            }
+
+            this.editTodo = () => {
+                todosService.editTodo(this.todo).then(() => {
+                    this.showEdit = false;
+                });
+            }
+        }
     });
--- app/services/todos.js
+++ app/services/todos.js
@@ -13,6 +13,14 @@
             return $http.post(baseUrl, todo);
         }
 
+        this.completeTodo = (todoId) => {
+            return $http.delete(baseUrl + todoId);
+        }
+
+        this.editTodo = (todo) => {
+            return $http.patch(baseUrl + todo._id, todo);
+        }
+
     }
 
     angular.module('manageBoard').service('todosService', injections);
