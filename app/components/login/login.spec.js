31130ffe320531dd8ec2d6e94f0c77c7e6d34e2b
Dmitriy Osetrov <dosetrov@yahoo.com>
2016-11-09 23:12:43 +0200

    Added dependencies to package.json

--- app/components/login/login.spec.js
+++ app/components/login/login.spec.js
@@ -1,4 +1,11 @@
-describe("A suite", function() {
+describe("Authorization Service Tests", function() {
+    var authorizationService, $httpBackend;
+
+    $$beforeEach(inject(function (_$httpBackend_, _authorizationService_) {
+        authorizationService = _authorizationService_;
+        $httpBackend = _$httpBackend_;
+    }));
+
     it("contains spec with an expectation", function() {
         expect(true).toBe(true);
     });
